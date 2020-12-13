1dcf1d06b13c8dff84830057151c18ec130091c4
siswarnava-sss <sinhaswarnava@gmail.com>
2020-12-13 20:57:00 +0530

    updated layout

--- app/src/main/java/com/example/college2/MainActivity.java
+++ app/src/main/java/com/example/college2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.college2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 
@@ -8,7 +9,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login2);
     }
 }
